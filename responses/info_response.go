3ed4aea30a1729e9db32af4806d3d9a8340edf43
Tristan Camejo <twisttaan@icloud.com>
2022-07-19 12:27:55 +1000

    refactor: cleaner info embeds

--- responses/info_response.go
+++ responses/info_response.go
@@ -2,12 +2,9 @@ package responses
 
 import (
 	"github.com/disgoorg/disgo/discord"
-	"time"
 )
 
 func NewInfoResponse() *discord.EmbedBuilder {
 	return discord.NewEmbedBuilder().
-		SetColor(0x2f3136).
-		SetTimestamp(time.Now()).
-		SetFooterText("$EVIE")
+		SetColor(0x2f3136)
 }
